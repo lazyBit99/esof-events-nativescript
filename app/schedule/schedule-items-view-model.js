495bd6c9d96166e5435d896e3ead363b8cb36db3
Alessandro <alessandro@macbook.local>
2019-12-12 18:22:02 +0100

    move data to file

--- app/schedule/schedule-items-view-model.js
+++ app/schedule/schedule-items-view-model.js
@@ -1,73 +1,16 @@
 const observableModule = require("tns-core-modules/data/observable");
+const data = require('./data')
+const redata = data.map((item) => {
+    item.sub = `${item.inizio} — ${item.fine} ${item.luogo}`
+    return item
+});
 
 function ScheduleItemsViewModel() {
     const viewModel = observableModule.fromObject({
-        items: [
-            {
-                name: "Colazione",
-                description: "8:00 - 9:00 Caffè degli Specchi"
-            },
-            {
-                name: "Item 2",
-                description: "Description for Item 2"
-            },
-            {
-                name: "Item 3",
-                description: "Description for Item 3"
-            },
-            {
-                name: "Item 4",
-                description: "Description for Item 4"
-            },
-            {
-                name: "Item 5",
-                description: "Description for Item 5"
-            },
-            {
-                name: "Item 6",
-                description: "Description for Item 6"
-            },
-            {
-                name: "Item 7",
-                description: "Description for Item 7"
-            },
-            {
-                name: "Item 8",
-                description: "Description for Item 8"
-            },
-            {
-                name: "Item 9",
-                description: "Description for Item 9"
-            },
-            {
-                name: "Item 10",
-                description: "Description for Item 10"
-            },
-            {
-                name: "Item 11",
-                description: "Description for Item 11"
-            },
-            {
-                name: "Item 12",
-                description: "Description for Item 12"
-            },
-            {
-                name: "Item 13",
-                description: "Description for Item 13"
-            },
-            {
-                name: "Item 14",
-                description: "Description for Item 14"
-            },
-            {
-                name: "Item 15",
-                description: "Description for Item 15"
-            },
-            {
-                name: "Item 16",
-                description: "Description for Item 16"
-            }
-        ]
+        items: redata,
+        myGroupingFunc: function(item) {
+            return item.group
+        }
     });
 
     return viewModel;
