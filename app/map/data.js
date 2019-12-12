f4ab930c9108cc47ea0110e43d3d6d703c165a24
Alessandro <alessandro@macbook.local>
2019-12-13 00:30:02 +0100

    better data file

--- app/map/data.js
+++ app/map/data.js
@@ -1,45 +1,65 @@
-
 module.exports = {
-    "type": "FeatureCollection",
-    "features": [
-      {
-        "type": "Feature",
-        "properties": {
-          "evento": "colazione"
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            13.762092590332031,
-            45.649268385921395
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {
-        "evento": "Inizio"
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            13.759925365447998,
-            45.646283314737836
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {
-          "evento": "fine"
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            13.764474391937254,
-            45.64815837802974
-          ]
-        }
-      }
-    ]
-  };
+	type: 'FeatureCollection',
+	features: [
+		{
+			type: 'Feature',
+			properties: {
+				icon:
+					'https://a.tiles.mapbox.com/v4/marker/pin-m-cafe+7044ff.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXFhYTA2bTMyeW44ZG0ybXBkMHkifQ.gUGbDOPUN1v1fTs5SeOR4A',
+				name: 'Caffè degli Specchi',
+			},
+			geometry: {
+				type: 'Point',
+				coordinates: [13.768411874771118, 45.65011212720475],
+			},
+		},
+		{
+			type: 'Feature',
+			properties: {
+				icon:
+					'https://a.tiles.mapbox.com/v4/marker/pin-m-town-hall+7044ff.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXFhYTA2bTMyeW44ZG0ybXBkMHkifQ.gUGbDOPUN1v1fTs5SeOR4A',
+				name: "Piazza Unità d'Italia",
+			},
+			geometry: {
+				type: 'Point',
+				coordinates: [13.767730593681335, 45.650044628369805],
+			},
+		},
+		{
+			type: 'Feature',
+			properties: {
+				icon:
+					'https://a.tiles.mapbox.com/v4/marker/pin-m-ferry+7044ff.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXFhYTA2bTMyeW44ZG0ybXBkMHkifQ.gUGbDOPUN1v1fTs5SeOR4A',
+				name: 'Porto Vecchio',
+			},
+			geometry: {
+				type: 'Point',
+				coordinates: [13.77006411552429, 45.6571203135106],
+			},
+		},
+		{
+			type: 'Feature',
+			properties: {
+				icon:
+					'https://a.tiles.mapbox.com/v4/marker/pin-m-restaurant+7044ff.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXFhYTA2bTMyeW44ZG0ybXBkMHkifQ.gUGbDOPUN1v1fTs5SeOR4A',
+				name: 'Eataly',
+			},
+			geometry: {
+				type: 'Point',
+				coordinates: [13.761371076107025, 45.64702209716792],
+			},
+		},
+		{
+			type: 'Feature',
+			properties: {
+				icon:
+					'https://a.tiles.mapbox.com/v4/marker/pin-m-town-hall+7044ff.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXFhYTA2bTMyeW44ZG0ybXBkMHkifQ.gUGbDOPUN1v1fTs5SeOR4A',
+				name: 'Piazza della Borsa',
+			},
+			geometry: {
+				type: 'Point',
+				coordinates: [13.77023845911026, 45.650378371816004],
+			},
+		},
+	],
+}
