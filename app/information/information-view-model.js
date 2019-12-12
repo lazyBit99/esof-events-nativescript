f25713b72298d3b6b3241442dffd9c7ad6b66b24
Alessandro <alessandro@macbook.local>
2019-12-12 18:14:37 +0100

    add model test

--- app/information/information-view-model.js
+++ app/information/information-view-model.js
@@ -3,6 +3,7 @@ const observableModule = require("tns-core-modules/data/observable");
 function InformationViewModel() {
     const viewModel = observableModule.fromObject({
         /* Add your view model properties here */
+        test: 'testo from model'
     });
 
     return viewModel;
