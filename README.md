bd8920e9290b1416904f6653b287ad157636fcea
Alessandro <alessandro@macbook.local>
2019-12-17 16:23:53 +0100

    update code

--- README.md
+++ README.md
@@ -1,19 +1,4 @@
 # ESOF Events 2020
 
-The **ESOF Events 2020** is a cross-platform mobile application made with [NativeScript](https://www.nativescript.org) available for *Android* and *iOS*. 
-
-![ESOF 2020 Logo](http://www.proesof2020.eu/ProxyVFS.axd/main_logo,image_logo/r15094/Logo-ESOF-x-sito-png?v=8290&ext=.png%20)
-
-## How to Start
-
-### Developer Mode
-To start developing run the following commands:
-```shell
-npm install
-```
-`npm run iphone` to lunch an iPhone 8
-*or*
-`npm run iphoneX` to lunch an iPhone X
-
-## Credits
-Developed by *ITS Kennedy*
+- npm install
+- npm run iphone
--- app/app.js
+++ app/app.js
@@ -1,12 +1,13 @@
-const application = require("tns-core-modules/application");
+const application = require('tns-core-modules/application')
+const config = require('./example.config')
+const APIKey = config.google.map.provideAPIKey || 'AIzaSyCP6f-dKamOLNSpy2nU34Xi4aQlB-sgNRo'
 
 // Google Maps SDK API KEY
-if(application.ios) {
-    GMSServices.provideAPIKey("AIzaSyCP6f-dKamOLNSpy2nU34Xi4aQlB-sgNRo");
+if (application.ios) {
+	GMSServices.provideAPIKey(APIKey)
 }
 
-
-application.run({ moduleName: "app-root" });
+application.run({ moduleName: 'app-root' })
 
 /*
 Do not place any code after the application has been started as it will not
