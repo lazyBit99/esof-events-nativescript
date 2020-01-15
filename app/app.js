1f24f46653db7e1b4b27a4f6e408a5ddf0e15208
Alessandro <alessandro@macbook.local>
2020-01-15 17:23:32 +0100

    init firebase module

--- app/app.js
+++ app/app.js
@@ -1,11 +1,27 @@
 const application = require('tns-core-modules/application')
-const config = require('./example.config')
-const APIKey = config.google.map.provideAPIKey || 'AIzaSyCP6f-dKamOLNSpy2nU34Xi4aQlB-sgNRo'
+const config = require('./config')
+const firebase = require('nativescript-plugin-firebase/app')
+const APIKey =
+	config.google.map.provideAPIKey || 'AIzaSyCP6f-dKamOLNSpy2nU34Xi4aQlB-sgNRo'
 
 // Google Maps SDK API KEY
 if (application.ios) {
 	GMSServices.provideAPIKey(APIKey)
 }
+
+firebase
+	.initializeApp({
+		iOSEmulatorFlush: false,
+		persist: false,
+	})
+	.then(
+		() => {
+			console.log('START')
+		},
+		error => {
+			console.error('ERROR' + error)
+		}
+	)
 
 application.run({ moduleName: 'app-root' })
 
