68959d2157020d49f0c512338dc2cc2dedcfe02c
Alessandro <alessandro@macbook.local>
2020-01-15 17:25:21 +0100

    get data from firestore and pull to refresh

--- app/schedule/schedule-sessions-view-model.js
+++ app/schedule/schedule-sessions-view-model.js
@@ -1,16 +1,42 @@
 const observableModule = require('tns-core-modules/data/observable')
-const data = require('./data')
+const ObservableArray = require('tns-core-modules/data/observable-array')
+	.ObservableArray
+const firebase = require('nativescript-plugin-firebase/app')
 
-const redata = data.map(session => {
-	session.subtitle = `${session.inizio} — ${session.fine} @ ${session.luogo}`
-	return session
-})
+let sessions = new ObservableArray()
+const schedule = firebase.firestore().collection('schedule')
+
+let fetch = () => {
+	schedule.get().then(documents => {
+		documents.forEach(document => {
+			let session = document.data()
+			session.subtitle = `${session.inizio} — ${session.fine} @ ${session.luogo}`
+			sessions.push(session)
+		})
+	})
+}
+
+fetch()
 
 function ScheduleSessionsViewModel() {
 	const viewModel = observableModule.fromObject({
-		sessions: redata,
+		sessions: sessions,
 		myGroupingFunc: session => {
 			return session.group
+		},
+		onPullToRefreshInitiated: args => {
+			console.log('updating...')
+
+			sessions = new ObservableArray()
+
+			let listView = args.object
+
+			fetch()
+			setTimeout(() => {
+				listView.notifyPullToRefreshFinished()
+			}, 1000)
+
+			console.log('DONE')
 		},
 	})
 
